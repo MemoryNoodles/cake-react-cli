2eecd8a05aa4d05c47336884394abbf184fc1eca
MemoryNoodles <729804002@qq.com>
2019-04-04 11:09:30 +0800

    .

--- bin/fileGenerate/configFile/importFile.js
+++
@@ -1,6 +0,0 @@
-module.exports = ()=>{
-return `import React from "react";
-        import { Route } from "react-router-dom";
-        import Loadable from "react-loadable";
-        import MyLoadingComponent from "~/components/common/loadComponents";`
-}
--- bin/template/commands/list.js
+++
@@ -1,9 +0,0 @@
-//webpack打包， 依赖的文件里在依赖其他文件 需要绝对路劲
-let tplJson = require(`${__dirname}\\..\\gitTemplate.json`);
-//let tplJson = require(`../gitTemplate`);
-const matter = require('gray-matter');
-var Table = require('cli-table3'); 
-
-module.exports = function(){
-    console.log(matter(JSON.stringify(tplJson)));
-}
